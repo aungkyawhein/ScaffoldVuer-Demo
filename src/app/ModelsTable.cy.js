0b03c2da16b628c7879e70b17fc2546bf55846b0
aungkyawhein <akh.infotech@gmail.com>
2023-09-25 17:25:43 +0800

    add cypress mount test

--- src/app/ModelsTable.cy.js
+++ src/app/ModelsTable.cy.js
@@ -2,7 +2,30 @@ import ModelsTable from './ModelsTable.vue';
 
 describe('<ModelsTable />', () => {
   it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(ModelsTable)
+    cy.mount(ModelsTable);
+  });
+});
+
+describe('Table Search and Sorting Test', () => {
+  beforeEach(() => {
+    cy.mount(ModelsTable);
+  });
+
+  // Search
+  it('should filter table rows when searching', () => {
+    // Wait for API data load
+
+    // Type a search query into the input field
+
+    // Verify that the table rows match the search query
+  });
+
+  // Sort
+  it('should sort table columns', () => {
+    // Trigger sort by key
+
+    // Verify that the table is sorted as expected
+
+    // Test for each key: Organs, Species, and Last modified
   });
 });
