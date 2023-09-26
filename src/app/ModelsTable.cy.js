c34ee8f51dc0be2a90fe0fa8a9198db1461cd055
aungkyawhein <akh.infotech@gmail.com>
2023-09-27 01:00:28 +0800

    add filter search test

--- src/app/ModelsTable.cy.js
+++ src/app/ModelsTable.cy.js
@@ -14,10 +14,97 @@ describe('Table Search and Sorting Test', () => {
   // Search
   it('should filter table rows when searching', () => {
     // Wait for API data load
+    // TODO - first run doesn't work because of API loading
+    // The data is from google spreadsheet
+    const totalRows = 28;
+    const TestOrgan = 'Bladder';
+    const TestOrganTotalRows = 4;
+    const TestSpecies = 'Rat';
+    const TestSpeciesTotalRows = 8;
+    const TestNote = 'Generic';
+    const TestNoteTotalRows = 10;
 
-    // Type a search query into the input field
+    // Alias
+    cy.get('input[placeholder="Type to search"]')
+      .as('searchInput');
+    cy.get('.el-table__body-wrapper tr.el-table__row')
+      .as('row');
+    cy.get('.el-table__body-wrapper tr.el-table__row td')
+      .first()
+      .as('firstCol');
+    cy.get('.el-table__body-wrapper tr.el-table__row td')
+      .eq(1)
+      .as('secondCol');
+    cy.get('.el-table__body-wrapper tr.el-table__row td')
+      .eq(2)
+      .as('thirdCol');
 
-    // Verify that the table rows match the search query
+    // All rows
+    cy.get('.el-table__body-wrapper')
+      .find('tr.el-table__row')
+      .should('have.length', totalRows);
+
+    // Test Organ
+    cy.get('@searchInput')
+      .type(TestOrgan);
+    cy.get('@firstCol')
+      .contains('Liver')
+      .should('not.exist');
+    cy.get('@firstCol')
+      .contains(TestOrgan)
+      .should('exist');
+    cy.get('@firstCol')
+      .contains('Brain')
+      .should('not.exist');
+    cy.get('@row')
+      .filter(`:contains(${TestOrgan})`)
+      .should('have.length', TestOrganTotalRows);
+
+    // Clear search
+    cy.get('@searchInput')
+      .clear();
+    cy.get('.el-table__body-wrapper')
+      .find('tr.el-table__row')
+      .should('have.length', totalRows);
+
+    // Test Species
+    cy.get('@searchInput')
+      .type(TestSpecies);
+    cy.get('@secondCol')
+      .contains('Liver')
+      .should('not.exist');
+    cy.get('@secondCol')
+      .contains(TestSpecies)
+      .should('exist');
+    cy.get('@secondCol')
+      .contains('Brain')
+      .should('not.exist');
+    cy.get('@row')
+      .filter(`:contains(${TestSpecies})`)
+      .should('have.length', TestSpeciesTotalRows);
+
+    // Clear search
+    cy.get('@searchInput')
+      .clear();
+    cy.get('.el-table__body-wrapper')
+      .find('tr.el-table__row')
+      .should('have.length', totalRows);
+
+    // Test Note
+    cy.get('@searchInput')
+      .type(TestNote);
+    cy.get('@thirdCol')
+      .contains('Liver')
+      .should('not.exist');
+    cy.get('@thirdCol')
+      .contains(TestNote)
+      .should('exist');
+    cy.get('@thirdCol')
+      .contains('Brain')
+      .should('not.exist');
+    cy.get('@row')
+      .filter(`:contains(${TestNote})`)
+      .should('have.length', TestNoteTotalRows);
   });
 
   // Sort
